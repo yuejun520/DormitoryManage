4d174116f0f9069a6bdafb489f53f10d8438a9a6
Luanhao <yuejun1314520@gmail.com>
2023-01-01 18:48:25 +0800

    控制层

---
+++ src/com/hbnu/controller/LoginServlet.java
@@ -0,0 +1,19 @@
+package com.hbnu.controller;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "LoginServlet", value = "/login")
+public class LoginServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        this.doPost(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
