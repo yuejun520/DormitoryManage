fd029322dd8e8d1e0f057b581a4a1fe10ff6b8cf
Luanhao <yuejun1314520@gmail.com>
2023-01-01 18:48:53 +0800

    数据库操作层

---
+++ src/com/hbnu/dao/IUserDao.java
@@ -0,0 +1,4 @@
+package com.hbnu.dao;
+
+public interface IUserDao {
+}
---
+++ src/com/hbnu/dao/impl/UserDAOImpl.java
@@ -0,0 +1,10 @@
+package com.hbnu.dao.impl;
+
+import com.hbnu.dao.IUserDao;
+
+/**
+ * @author luanhao
+ * @date 2023年01月01日 18:28
+ */
+public class UserDAOImpl implements IUserDao {
+}
